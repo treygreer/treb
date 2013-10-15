ba226b89b24f17c197ca5c2327f510b9c1e32135
Trey Greer <tgreer@nvidia.com>
2013-10-15 16:12:45 -0400

    porting animation to PyQt4 (not done)

--- treb_sim/src/dynamics/animation.py
+++ treb_sim/src/dynamics/animation.py
@@ -1,55 +1,61 @@
-import PyQt4
-pygtk.require('2.0')
-import gtk
-import gtk.gdk
+from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import Qt
 from dynamics.constants import meter2foot
-from numpy import arange
-import numpy
-import cv
+import numpy as np
+#import cv  # for movie creation
 
-# Create a GTK+ widget on which we will draw using Cairo
-class Animation:
-    def __init__(self, sim, dist, movie_flag=False):
-        self.sim, self.dist, self.movie_flag = sim, dist, movie_flag
+class EnergiesBox(QtGui.QGroupBox):
+    def __init(self):
+        super().__init__('Energies')
 
-        self.window = gtk.Window()
-        self.vbox = gtk.VBox(homogeneous=False, spacing=1)
-
-        self.time_adj = gtk.Adjustment(value=0, lower=0, upper=(sim.Y.shape[0]-1)*sim.time_step,
-                                       step_incr=sim.time_step, page_incr=10*sim.time_step)
-        time_scale = gtk.HScale(self.time_adj)
-        time_scale.set_digits(3)
-        time_scale.set_value_pos(gtk.POS_LEFT)
-
-        draw_forces = gtk.CheckButton(label="Draw Force Vectors")
-
-        energies_frame = gtk.Frame(label="Energies")
-        energies_table = gtk.Table(rows=len(sim.frames), columns=2, homogeneous=False)
+        energies_table = gtk.Table(rows=len(sim.frames), columns=2,
+                                   homogeneous=False)
         energies_frame.add(energies_table)
 
         row = 0
         for frame in sim.frames:
             label = gtk.Label(frame.name)
-            energies_table.attach(label, 0, 1, row, row+1, xoptions=gtk.SHRINK, xpadding=2)
+            energies_table.attach(label, 0, 1, row, row+1, xoptions=gtk.SHRINK,
+                                  xpadding=2)
             label.show()
             frame.energy_bar = EnergyBar(sim, frame)
-            energies_table.attach(frame.energy_bar, 1, 2, row, row+1, xoptions=gtk.EXPAND,
+            energies_table.attach(frame.energy_bar, 1, 2, row, row+1,
+                                  xoptions=gtk.EXPAND,
                                   xpadding=2, ypadding=2)
             frame.energy_bar.show()
             row=row+1
         energies_table.show()
-        self.sim.total_energy = sum([frame.KE() + frame.PE() - frame.PEmin for frame in sim.frames])
+        self.sim.total_energy = sum([frame.KE() + frame.PE() - frame.PEmin
+                                      for frame in sim.frames])
 
-        range_frame = gtk.Frame(label="Range")
+class RangeBox(QtGui.QGroupBox):
+    def __init(self):
+        super().__init__('Range')
         self.range_bar = gtk.ProgressBar()
         self.range_bar.show()
         range_frame.add(self.range_bar)
 
+class Animation(QtGui.QVBoxLayout):
+    def __init__(self, sim, dist, movie_flag=False):
+        super().__init__()
+        self.sim, self.dist, self.movie_flag = sim, dist, movie_flag
+
+        time_slider = QtGui.QSlider(Qt.Horizontal)
+        time_slider.setRange(0, sim.Y.shape[0])
+        time_slider.setValue(0)
+        time_slider.setPageStep(sim.Y.shape[0]/100)
+        time_slider.setSingleStep(1)
+
+        draw_forces_button = QtGui.QCheckBox("Draw Force Vectors")
+
+        energies_frame = EnergiesBox()
+        range_frame = RangeBox()
+
         self.drawing=Drawing(self.time_adj, draw_forces, sim)
         self.vbox.pack_start(self.drawing, expand=True)
         self.drawing.show()
-        self.vbox.pack_end(time_scale, expand=False)
-        time_scale.show()
+        self.vbox.pack_end(time_slider, expand=False)
+        time_slider.show()
         self.vbox.pack_end(energies_frame, expand=False)
         energies_frame.show()
         self.vbox.pack_end(range_frame, expand=False)
@@ -74,37 +80,35 @@ class Animation:
 
     def update_range(self):
         range_ = self.dist(self.sim.Y[self.sim.time_idx])
-        maxrange = numpy.max(self.dist(self.sim.Y))
+        maxrange = np.max(self.dist(self.sim.Y))
         fraction = range_/maxrange
         if fraction < 0:
             fraction = 0
         self.range_bar.set_fraction(fraction)
         self.range_bar.set_text("%g feet" % (meter2foot(range_)))
-
+    '''
     def movie(self):
         #create a video writer
         width, height = self.vbox.window.get_size()
         writer = cv.CreateVideoWriter('movie.avi', -1, 60, (width, height), is_color=1)
         gtk.gdk.window_process_all_updates()
-        for time in arange(0.0, 1.0, 0.001):
+        for time in np.arange(0.0, 1.0, 0.001):
             self.time_adj.set_value(time)
             gtk.gdk.window_process_all_updates()
             pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, width, height)
             pb = pb.get_from_drawable(self.vbox.window, self.vbox.window.get_colormap(), 0, 0, 0, 0, width, height)
             pixel_str = pb.get_pixels()
-            pixel_array = numpy.fromstring(pixel_str, dtype=numpy.uint8)
+            pixel_array = np.fromstring(pixel_str, dtype=np.uint8)
             pixel_array = pixel_array.reshape((height, width, 3), order='C')
             cv_mat = cv.fromarray(pixel_array)
             #cv_frame = cv.DecodeImage(cv_mat, iscolor=1)
             cv.WriteFrame(writer, cv_mat)
+    '''
 
 
-class EnergyBar(gtk.DrawingArea):
-    # Draw in response to an expose-event
-    __gsignals__ = { "expose-event": "override" }
-
+class EnergyBar(QtGui.QWidget):
     def __init__(self, sim, frame):
-        gtk.DrawingArea.__init__(self)
+        super().__init__()
         self.sim, self.frame = sim, frame
         self.set_size_request(400,20)
 
@@ -124,7 +128,8 @@ class EnergyBar(gtk.DrawingArea):
         self.cr.fill()
 
         # draw potential energy bar
-        potential_fraction = (self.frame.PE() - self.frame.PEmin) / self.sim.total_energy
+        potential_fraction = (self.frame.PE() - 
+                              self.frame.PEmin) / self.sim.total_energy
         self.cr.set_source_rgb(0.1,1.0,0.1)
         self.cr.rectangle(0, 0, w*potential_fraction, h)
         self.cr.fill()
@@ -135,11 +140,9 @@ class EnergyBar(gtk.DrawingArea):
         self.cr.rectangle(w*potential_fraction, 0, w*kinetic_fraction, h)
         self.cr.fill()
 
-class Drawing(gtk.DrawingArea):
-    # Draw in response to an expose-event
-    __gsignals__ = { "expose-event": "override" }
-
+class Drawing(QtGui.QWidget):
     def __init__(self, time_adj, draw_forces, sim):
+        super().__init__()
         gtk.DrawingArea.__init__(self)
         self.time_adj, self.draw_forces, self.sim = time_adj,draw_forces,sim
         self.set_size_request(400,400)
@@ -176,7 +179,8 @@ class Drawing(gtk.DrawingArea):
         #print "time=", time, "time_idx=", self.sim.time_idx
         c=0
         for constraint in self.sim.constraints:
-            constraint.enabled = self.sim.constraints_enabled[self.sim.time_idx,c]
+            constraint.enabled = self.sim.constraints_enabled \
+                [self.sim.time_idx,c]
             c=c+1
         self.sim._deriv(time, self.sim.Y[self.sim.time_idx])
 
