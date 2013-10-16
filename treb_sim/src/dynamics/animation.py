e4fb6c3cfea8f6b4f75aa28c35fa0f3387d7625b
treygreer <greer@treytronics.org>
2013-10-16 07:26:35 -0400

    more progress on PyQt4 port

--- treb_sim/src/dynamics/animation.py
+++ treb_sim/src/dynamics/animation.py
@@ -4,119 +4,14 @@ from dynamics.constants import meter2foot
 import numpy as np
 #import cv  # for movie creation
 
-class EnergiesBox(QtGui.QGroupBox):
-    def __init(self):
-        super().__init__('Energies')
-
-        energies_table = gtk.Table(rows=len(sim.frames), columns=2,
-                                   homogeneous=False)
-        energies_frame.add(energies_table)
-
-        row = 0
-        for frame in sim.frames:
-            label = gtk.Label(frame.name)
-            energies_table.attach(label, 0, 1, row, row+1, xoptions=gtk.SHRINK,
-                                  xpadding=2)
-            label.show()
-            frame.energy_bar = EnergyBar(sim, frame)
-            energies_table.attach(frame.energy_bar, 1, 2, row, row+1,
-                                  xoptions=gtk.EXPAND,
-                                  xpadding=2, ypadding=2)
-            frame.energy_bar.show()
-            row=row+1
-        energies_table.show()
-        self.sim.total_energy = sum([frame.KE() + frame.PE() - frame.PEmin
-                                      for frame in sim.frames])
-
-class RangeBox(QtGui.QGroupBox):
-    def __init(self):
-        super().__init__('Range')
-        self.range_bar = gtk.ProgressBar()
-        self.range_bar.show()
-        range_frame.add(self.range_bar)
-
-class Animation(QtGui.QVBoxLayout):
-    def __init__(self, sim, dist, movie_flag=False):
-        super().__init__()
-        self.sim, self.dist, self.movie_flag = sim, dist, movie_flag
-
-        time_slider = QtGui.QSlider(Qt.Horizontal)
-        time_slider.setRange(0, sim.Y.shape[0])
-        time_slider.setValue(0)
-        time_slider.setPageStep(sim.Y.shape[0]/100)
-        time_slider.setSingleStep(1)
-
-        draw_forces_button = QtGui.QCheckBox("Draw Force Vectors")
-
-        energies_frame = EnergiesBox()
-        range_frame = RangeBox()
-
-        self.drawing=Drawing(self.time_adj, draw_forces, sim)
-        self.vbox.pack_start(self.drawing, expand=True)
-        self.drawing.show()
-        self.vbox.pack_end(time_slider, expand=False)
-        time_slider.show()
-        self.vbox.pack_end(energies_frame, expand=False)
-        energies_frame.show()
-        self.vbox.pack_end(range_frame, expand=False)
-        range_frame.show()
-        self.vbox.pack_end(draw_forces, expand=False)
-        draw_forces.show()
-        self.time_adj.connect("value_changed", lambda w: self.draw())
-        draw_forces.connect("toggled", lambda w: self.draw())
-        self.update_range()
-
-        self.window.add(self.vbox)
-        self.vbox.show()
-        self.window.present()
-        gtk.main()
-
-    def draw(self):
-        self.drawing.draw()
-        for frame in self.sim.frames:
-            frame.energy_bar.draw()
-        self.update_range()
-
-
-    def update_range(self):
-        range_ = self.dist(self.sim.Y[self.sim.time_idx])
-        maxrange = np.max(self.dist(self.sim.Y))
-        fraction = range_/maxrange
-        if fraction < 0:
-            fraction = 0
-        self.range_bar.set_fraction(fraction)
-        self.range_bar.set_text("%g feet" % (meter2foot(range_)))
-    '''
-    def movie(self):
-        #create a video writer
-        width, height = self.vbox.window.get_size()
-        writer = cv.CreateVideoWriter('movie.avi', -1, 60, (width, height), is_color=1)
-        gtk.gdk.window_process_all_updates()
-        for time in np.arange(0.0, 1.0, 0.001):
-            self.time_adj.set_value(time)
-            gtk.gdk.window_process_all_updates()
-            pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, width, height)
-            pb = pb.get_from_drawable(self.vbox.window, self.vbox.window.get_colormap(), 0, 0, 0, 0, width, height)
-            pixel_str = pb.get_pixels()
-            pixel_array = np.fromstring(pixel_str, dtype=np.uint8)
-            pixel_array = pixel_array.reshape((height, width, 3), order='C')
-            cv_mat = cv.fromarray(pixel_array)
-            #cv_frame = cv.DecodeImage(cv_mat, iscolor=1)
-            cv.WriteFrame(writer, cv_mat)
-    '''
-
-
 class EnergyBar(QtGui.QWidget):
     def __init__(self, sim, frame):
         super().__init__()
         self.sim, self.frame = sim, frame
-        self.set_size_request(400,20)
-
-    # Handle the expose-event by drawing
-    def do_expose_event(self, event):
-        self.draw()
+        #self.set_size_request(400,20)
 
     def draw(self):
+        return
         # Create the cairo context
         self.cr = self.window.cairo_create()
         w,h = self.window.get_size()
@@ -140,19 +35,107 @@ class EnergyBar(QtGui.QWidget):
         self.cr.rectangle(w*potential_fraction, 0, w*kinetic_fraction, h)
         self.cr.fill()
 
+class EnergiesBox(QtGui.QGroupBox):
+    def __init__(self, sim):
+        super().__init__('Energies')
+        grid = QtGui.QGridLayout()
+        row = 0
+        for frame in sim.frames:
+            label = QtGui.QLabel(frame.name)
+            grid.addWidget(label, 0, row)
+            label.show()
+            frame.energy_bar = EnergyBar(sim, frame)
+            grid.addWidget(frame.energy_bar, 1, row)
+            row=row+1
+        self.setLayout(grid)
+
+class RangeBox(QtGui.QGroupBox):
+    def __init__(self, max_range):
+        super().__init__('Range')
+        self.range_bar = QtGui.QProgressBar()
+        self.range_bar.setRange(0, int(max_range))
+        hbox = QtGui.QHBoxLayout()
+        self.text_box = QtGui.QLabel()
+        hbox.addWidget(self.text_box)
+        hbox.addWidget(self.range_bar)
+        self.setLayout(hbox)
+    def set_range(self, range_):
+        self.range_bar.setValue(int(range_))
+        self.text_box.setText("%g feet" % (meter2foot(range_)))
+
+class TimeSlider(QtGui.QSlider):
+    def __init__(self, sim):
+        super().__init__(Qt.Horizontal)
+        self.setRange(0, sim.Y.shape[0])
+        self.setValue(0)
+        self.setPageStep(sim.Y.shape[0]/100)
+        self.setSingleStep(1)
+
+class Animation(QtGui.QWidget):
+    def __init__(self, sim, dist, movie_flag=False):
+        super().__init__()
+        self.sim, self.dist, self.movie_flag = sim, dist, movie_flag
+        self.sim.total_energy = sum([frame.KE() + frame.PE() - frame.PEmin
+                                      for frame in sim.frames])
+
+        vbox = QtGui.QVBoxLayout()
+        time_slider = TimeSlider(sim)
+        draw_forces_button = QtGui.QCheckBox("Draw Force Vectors")
+        self.drawing=Drawing(time_slider, draw_forces_button, sim)
+        energies_box = EnergiesBox(sim)
+        self.range_box = RangeBox(np.max(self.dist(self.sim.Y)))
+
+        vbox.addWidget(self.drawing)
+        vbox.addWidget(time_slider)
+        vbox.addWidget(energies_box)
+        vbox.addWidget(self.range_box)
+        vbox.addWidget(draw_forces_button)
+        self.setLayout(vbox)
+
+        time_slider.valueChanged.connect(self.draw)
+        draw_forces_button.stateChanged.connect(self.draw)
+
+        self.update_range()
+
+        self.show()
+
+    def draw(self):
+        self.drawing.draw()
+        for frame in self.sim.frames:
+            frame.energy_bar.draw()
+        self.update_range()
+
+    def update_range(self):
+        self.range_box.set_range(self.dist(self.sim.Y[self.sim.time_idx]))
+    '''
+    def movie(self):
+        #create a video writer
+        width, height = self.vbox.window.get_size()
+        writer = cv.CreateVideoWriter('movie.avi', -1, 60, (width, height), is_color=1)
+        gtk.gdk.window_process_all_updates()
+        for time in np.arange(0.0, 1.0, 0.001):
+            self.time_adj.set_value(time)
+            gtk.gdk.window_process_all_updates()
+            pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, width, height)
+            pb = pb.get_from_drawable(self.vbox.window, self.vbox.window.get_colormap(), 0, 0, 0, 0, width, height)
+            pixel_str = pb.get_pixels()
+            pixel_array = np.fromstring(pixel_str, dtype=np.uint8)
+            pixel_array = pixel_array.reshape((height, width, 3), order='C')
+            cv_mat = cv.fromarray(pixel_array)
+            #cv_frame = cv.DecodeImage(cv_mat, iscolor=1)
+            cv.WriteFrame(writer, cv_mat)
+    '''
+
+
 class Drawing(QtGui.QWidget):
     def __init__(self, time_adj, draw_forces, sim):
         super().__init__()
-        gtk.DrawingArea.__init__(self)
         self.time_adj, self.draw_forces, self.sim = time_adj,draw_forces,sim
-        self.set_size_request(400,400)
+        #self.set_size_request(400,400)
         self.sim.time_idx = 0
 
-    # Handle the expose-event by drawing
-    def do_expose_event(self, event):
-        self.draw()
-
     def draw(self):
+        return
         # Create the cairo context
         self.cr = self.window.cairo_create()
 
